743d0c5ef1670d59de6412826f27fc376593e328
StackTheCode <kamran.rahmanov2005@gmail.com>
2025-12-13 12:25:44 +0000

    test files

---
+++ ml-engine/test_image_upload.py
@@ -0,0 +1,16 @@
+import requests
+# poetry run uvicorn main:app --reload
+# poetry run python test_image_upload.py
+image_path = "C:\\Users\\user\Desktop\\test_image.jpg"
+with open(image_path,'rb') as f :
+    files = {'file':('image.jpg',f,'image/jpeg' )}
+    response = requests.post('http://localhost:8000/analyze-posture', files=files)
+    
+    
+    result = response.json()
+    print(f"Posture: {result['posture_state']}")
+    print(f"Confidence: {result['confidence']:.2f}")
+    print(f"Severity: {result['severity']:.2f}")
+    print(f"Recommendations:")
+    for tip in result['recommendations']:
+      print(f"   - {tip}")
