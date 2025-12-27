30713b4cbdf880063e27bedc83fdcfa3616ada24
StackTheCode <kamran.rahmanov2005@gmail.com>
2025-12-27 19:26:46 +0000

    header fix of backendClient

--- desktop-agent/src/network/BackendClient.cpp
+++ desktop-agent/src/network/BackendClient.cpp
@@ -24,7 +24,7 @@ bool BackendClient::sendPostureEvent(std::string &postureState, double confidenc
     auto now = std::chrono::system_clock::now();
     auto time = std::chrono::system_clock::to_time_t(now);
     std::stringstream ss;
-    ss << std::put_time(std::gmtime(&time), "%Y -%m-%dT%H:%M:%S");
+    ss << std::put_time(std::gmtime(&time), "%Y-%m-%dT%H:%M:%S");
     std::string timestamp = ss.str();
 
     json payload = {
@@ -43,12 +43,21 @@ bool BackendClient::sendPostureEvent(std::string &postureState, double confidenc
     headers = curl_slist_append(headers, authHeader.c_str());
 
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_HEADER, headers);
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonStr.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
 
+   std::cout << "Sending to: " << url << std::endl;
+    std::cout << "Payload: " << jsonStr << std::endl;
+    std::cout << "Token: " << token.substr(0, 20) << "..." << std::endl;
+
     CURLcode res = curl_easy_perform(curl);
+
+ long http_code = 0;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    std::cout << "HTTP Response Code: " << http_code << std::endl;
+    std::cout << "Response Body: " << readBuffer << std::endl;
 
     curl_slist_free_all(headers);
     curl_easy_cleanup(curl);
