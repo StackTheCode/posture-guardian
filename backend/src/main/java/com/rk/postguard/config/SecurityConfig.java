799c3c0aaa0332217e17768b46681387fe5ecd3e
StackTheCode <kamran.rahmanov2005@gmail.com>
2026-02-25 21:10:22 +0000

    backend fix

--- backend/src/main/java/com/rk/postguard/config/SecurityConfig.java
+++ backend/src/main/java/com/rk/postguard/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**","/api/v1/users/**","/api/v1/analytics/**","/ping","/health","/actuator/**" ,"/ws/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**","/api/v1/analytics/**","/ping","/health","/actuator/**" ,"/ws/**").permitAll()
+                        .requestMatchers("/api/v1/users/**").authenticated()
                         .anyRequest().authenticated()
                 )
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
